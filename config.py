dc034912007bc4ed17c73334052d786ad777b685
Christian-web99 <parkwoghd11@naver.com>
2025-04-21 20:25:32 +0900

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# Configuration and API keys
-
-import os
-
-# ✅ 텔레그램 봇 설정
-os.environ["TELEGRAM_TOKEN"] = "7662927522:AAGb5ak46iXEcMXxNrT_icJuHrmNjt88Uk4"
-os.environ["TELEGRAM_CHAT_ID"] = "7889848921,7807026817"
